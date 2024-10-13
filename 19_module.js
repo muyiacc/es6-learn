d3db5f6fb5a397fee2cc00c539729531042f86d0
muyiacc <115904774+muyiacc@users.noreply.github.com>
2024-10-13 10:43:24 +0800

    init

---
+++ 19_module.js
@@ -0,0 +1,14 @@
+let title = "寻道之旅"
+let web = "seektao.cc"
+
+let getWeb = () => "www.seektao.cc"
+
+// 第一种
+// export {title, web, getWeb}
+
+// 第二种
+// export default {title, web, getWeb}
+
+
+// 第三种
+export {title, web, getWeb}
